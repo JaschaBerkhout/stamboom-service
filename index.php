ecb41ad9c50222b2faa89d524f5d10cf25522cc5
daan.rijpkema <daan.rijpkema@coolblue.nl>
2023-01-10 16:52:30 +0100

    First classes and explanations

--- index.php
+++ index.php
@@ -2,64 +2,117 @@
 require 'vendor/autoload.php';
 use App\SQLiteConnection;
 
-$pdo = (new SQLiteConnection())->connect();
-if ($pdo != null)
-    echo 'Connected to the SQLite database successfully!';
-else
-    echo 'Whoops, could not connect to the SQLite database!';
+function laatAllePersoontjesZienVoorUser($persons, $userId) {
+    echo "<br>Personen lijst: <hr>";
+    echo "<pre>";
+    print_r($persons);
+    echo "</pre>";
+}
 
-//     $stmt = $pdo->query("SELECT * FROM persons");
-// while ($row = $stmt->fetch()) {
-//     print_r($row);
-// }
-
-
-$persons = [$person1, $person2];
-
-
-function getPersonsPerUser($userId){
-    global $persons;
+function getPersonsPerUser($pdo, $userId){
+    $stmt = $pdo->query("SELECT * FROM persons");
+    $persons = [];
+    while ($row = $stmt->fetch()) {
+        $person = [
+            'f_name' => $row['f_name'],
+            'l_name' => $row['l_name'],
+            'gender' => $row['gender'],
+            'birthday' => $row['birthday'],
+            'user_id' => $row['user_id'], // later automatisch opvragen
+        ];
+        $persons[] = $person;
+    }
     return $persons;
 }
-$get_persons_per_user = getPersonsPerUser(1);
+
+// meer functies
+
+//query persoon toevoegen
+function insertPerson($pdo, $person)
+{
+    $data = [
+        'f_name' => $person['f_name'],
+        'l_name' => $person['l_name'],
+        'gender' => $person['gender'],
+        'birthday' => $person['birthday'],
+        'user_id' => $person['user_id'], // later automatisch opvragen
+    ];
+
+    $sql = "INSERT INTO persons (f_name, l_name, gender,birthday, user_id) VALUES (:f_name, :l_name, :gender,:birthday, :user_id)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($data);
+}
+
+//query persoon verwijderen
+function removePerson($pdo, $id)
+{
+    $data = [
+        'id' => $id,
+    ];
+    $sql = "DELETE FROM persons (id) VALUES (:id)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($data);
+}
+
+
+
+$db = new PersonenDatabase();
+$db->insertUser('henkie', 'xxhorses123');
+
+$person = [
+    "f_name" => "Jascha",
+    "l_name" => "Berkiewood",
+    "gender"=> "F",
+    "birthday" => "16-02-1995",
+    "user_id"=>1,
+];
+
+// @todo: deze functies werken nu niet, die moeten nog verplaatst worden!!
+// @todo: bedenk een tweede class om te gebruiken in deze file.
+
+
+$persons = getPersonsPerUser($pdo, 1);
+insertPerson($pdo, $person);
+
+//removePerson($pdo, $person['id']);
+//laatAllePersoontjesZienVoorUser($pdo, 1);
+
+
 
 $first_person = $get_persons_per_user[0];
 
 
-// meer functies
 
-//query user toevoegen
-$data = [
-    'user_name' => $user_name,
-    'password' => $password,
-];
-$sql = "INSERT INTO persons (user_name, password) VALUES (:user_name, :password)";
-$stmt= $pdo->prepare($sql);
-$stmt->execute($data);
-
-//query persoon toevoegen
-$data = [
-    'f_name' => $f_name,
-    'l_name' => $l_name,    
-    'gender' => $gender,
-    'birthday' => $birthday,
-    'user_id' => $user_id,
-];
-$sql = "INSERT INTO persons (f_name, l_name, gender,birthday, user_id) VALUES (:f_name, :l_name, :gender,:birthday, :user_id)";
-$stmt= $pdo->prepare($sql);
-$stmt->execute($data);
-
-//query persoon verwijderen
-$data = [
-    'id' => $id,
-];
-$sql = "DELETE FROM persons (id) VALUES (:id)";
-$stmt= $pdo->prepare($sql);
-$stmt->execute($data);
-
+laatAllePersoontjesZienVoorUser($persons, 1);
 // objecten
 
 // classes
+class PersonenDatabase {
+    private $pdo;
+
+    public function __construct() {
+
+        $this->pdo = (new SQLiteConnection())->connect();
+        if ($this->pdo != null) {
+            echo 'Connected to the SQLite database successfully!';
+        } else {
+            echo 'Whoops, could not connect to the SQLite database!';
+        }
+    }
+
+    //query user toevoegen
+    public function insertUser($user_name, $password)
+    {
+        $data = [
+            'user_name' => $user_name,
+            'password' => $password,
+        ];
+        $sql = "INSERT INTO users (user_name, password) VALUES (:user_name, :password)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($data);
+    }
+}
+
 
 // functies in objecten en class instantiatie
 
@@ -73,4 +126,6 @@ $stmt->execute($data);
 // Requests GET
 // JSON
 // requests POST
+
+
 
