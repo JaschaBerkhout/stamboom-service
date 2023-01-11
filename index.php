29faedf501e47236cf5660d7fd104174b547b9e7
JaschaBerkhout <76992119+JaschaBerkhout@users.noreply.github.com>
2023-01-11 10:05:02 +0100

    aanpassingen voor pdo class

--- index.php
+++ index.php
@@ -2,61 +2,14 @@
 require 'vendor/autoload.php';
 use App\SQLiteConnection;
 
-function laatAllePersoontjesZienVoorUser($persons, $userId) {
+function visualizePersonsPerUser($persons, $userId) {
     echo "<br>Personen lijst: <hr>";
     echo "<pre>";
     print_r($persons);
     echo "</pre>";
 }
 
-function getPersonsPerUser($pdo, $userId){
-    $stmt = $pdo->query("SELECT * FROM persons");
-    $persons = [];
-    while ($row = $stmt->fetch()) {
-        $person = [
-            'f_name' => $row['f_name'],
-            'l_name' => $row['l_name'],
-            'gender' => $row['gender'],
-            'birthday' => $row['birthday'],
-            'user_id' => $row['user_id'], // later automatisch opvragen
-        ];
-        $persons[] = $person;
-    }
-    return $persons;
-}
-
-// meer functies
-
-//query persoon toevoegen
-function insertPerson($pdo, $person)
-{
-    $data = [
-        'f_name' => $person['f_name'],
-        'l_name' => $person['l_name'],
-        'gender' => $person['gender'],
-        'birthday' => $person['birthday'],
-        'user_id' => $person['user_id'], // later automatisch opvragen
-    ];
-
-    $sql = "INSERT INTO persons (f_name, l_name, gender,birthday, user_id) VALUES (:f_name, :l_name, :gender,:birthday, :user_id)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute($data);
-}
-
-//query persoon verwijderen
-function removePerson($pdo, $id)
-{
-    $data = [
-        'id' => $id,
-    ];
-    $sql = "DELETE FROM persons (id) VALUES (:id)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute($data);
-}
-
-
-
-$db = new PersonenDatabase();
+$db = new PersonsDatabase();
 $db->insertUser('henkie', 'xxhorses123');
 
 $person = [
@@ -67,27 +20,20 @@ $person = [
     "user_id"=>1,
 ];
 
-// @todo: deze functies werken nu niet, die moeten nog verplaatst worden!!
-// @todo: bedenk een tweede class om te gebruiken in deze file.
-
-
-$persons = getPersonsPerUser($pdo, 1);
-insertPerson($pdo, $person);
-
-//removePerson($pdo, $person['id']);
-//laatAllePersoontjesZienVoorUser($pdo, 1);
-
-
+// @todo: bedenk een tweede class om te gebruiken in deze file. > 
 
 $first_person = $get_persons_per_user[0];
 
+visualizePersonsPerUser($persons, 1);
+$dp->removePerson($person['id']);
 
+$persons = $dp->getPersonsPerUser(1);
+$dp->insertPerson($person);
 
-laatAllePersoontjesZienVoorUser($persons, 1);
 // objecten
 
 // classes
-class PersonenDatabase {
+class PersonsDatabase {
     private $pdo;
 
     public function __construct() {
@@ -110,6 +56,50 @@ class PersonenDatabase {
         $sql = "INSERT INTO users (user_name, password) VALUES (:user_name, :password)";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute($data);
+    }
+// @todo: deze functies werken nu niet, die moeten nog verplaatst worden!!
+    
+    //query persoon toevoegen
+    function insertPerson($pdo, $person)
+    {
+        $data = [
+            'f_name' => $person['f_name'],
+            'l_name' => $person['l_name'],
+            'gender' => $person['gender'],
+            'birthday' => $person['birthday'],
+            'user_id' => $person['user_id'], // later automatisch opvragen
+        ];
+
+        $sql = "INSERT INTO persons (f_name, l_name, gender,birthday, user_id) VALUES (:f_name, :l_name, :gender,:birthday, :user_id)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($data);
+    }
+
+    //query persoon verwijderen
+    function removePerson($pdo, $id)
+    {
+        $data = [
+            'id' => $id,
+        ];
+        $sql = "DELETE FROM persons (id) VALUES (:id)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($data);
+    }
+
+    function getPersonsPerUser($pdo, $userId){
+        $stmt = $pdo->query("SELECT * FROM persons");
+        $persons = [];
+        while ($row = $stmt->fetch()) {
+            $person = [
+                'f_name' => $row['f_name'],
+                'l_name' => $row['l_name'],
+                'gender' => $row['gender'],
+                'birthday' => $row['birthday'],
+                'user_id' => $row['user_id'], // later automatisch opvragen
+            ];
+            $persons[] = $person;
+        }
+        return $persons;
     }
 }
 
