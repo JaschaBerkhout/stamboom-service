2de67525fe124f086bf03172f221c18614e6dcfd
daan.rijpkema <daan.rijpkema@coolblue.nl>
2023-01-12 15:32:55 +0100

    Cleaning up my classes

--- index.php
+++ index.php
@@ -7,16 +7,27 @@ use App\Presenter;
 $db = new PersonsDatabase();
 $presenter = new Presenter();
 
-$users = $db->getUsers();
-$presenter->displayUsers($users);
+
+
+print_r($_GET);
+$type = $_GET['type'];
+echo $type;
+echo "<HR>";
+
+if ($type === 'users') {
+    $users = $db->getUsers();
+    $presenter->displayUsers($users);
+} elseif ($type === 'persons') {
+    displayPersonsFromUser($db, $presenter, 1);
+} else {
+    exit("Je hebt geen geldig type ingevuld jochie!");
+}
+
 
 function displayPersonsFromUser($db, $presenter, $user_id) {
     $persons = $db->getPersonsPerUser($user_id);
     echo "Displaying persons from $user_id";
     $presenter->displayPersons($persons);
 }
-
-displayPersonsFromUser($db, $presenter, 1);
-displayPersonsFromUser($db, $presenter, 70);
 
 
